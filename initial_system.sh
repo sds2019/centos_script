c0e32ed88b0867ece65fbdc2cc720cad70b8f3c4
sds2019 <49633357+sds2019@users.noreply.github.com>
2019-07-08 13:29:46 +0800

    fix echo bug

--- initial_system.sh
+++ initial_system.sh
@@ -1,13 +1,13 @@
 # tested in centos 7.6+
 #!/bin/bash
 yum update -y
-echo "=====================================updated."
+echo "=====================================updated.\n\n\n"
 yum install -y rpm-build 
-echo "======================================installed: rpm-build"
+echo "======================================installed: rpm-build\n\n\n"
 yum install unzip -y
-echo "======================================installed: unzip"
+echo "======================================installed: unzip\n\n\n"
 yum install wget -y
-echo "======================================installed: wget"
+echo "======================================installed: wget\n\n\n"
 wget http://192.168.18.2:8080/artifactory/libs-release-local/orcale/jdk-8u191-linux-x64.tar.gz
 tar xvfz jdk-8u191-linux-x64.tar.gz
 mkdir -p /usr/lib/jvm/
@@ -18,19 +18,19 @@ echo "export CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar" >> /etc
 chmod 0744 /etc/profile.d/java.sh
 source /etc/profile.d/java.sh
 java -version
-ech "========================================installed: jdk"
+echo "========================================installed: jdk\n\n\n"
 yum install vim -y
 echo "alias vi=vim" >> /etc/profile
 yum install gcc -y
 yum install gcc-c++ -y
 yum install gdb -y
-echo "=====================================gcc gdb g++ installed."
+echo "=====================================gcc gdb g++ installed.\n\n\n"
 wget http://www.roland-riegel.de/nload/nload-0.7.2.tar.gz 
 tar zxvf nload-0.7.2.tar.gz 
 wget http://mirrors.kernel.org/fedora-epel/6/i386/epel-release-6-8.noarch.rpm
 rpm -ivh epel-release-6-8.noarch.rpm
 yum install nload -y
-echo "=====================================nload installed."
+echo "=====================================nload installed.\n\n\n"
 echo "begin to install python 3"
 yum install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel -y
 yum install libffi-devel -y
@@ -43,14 +43,14 @@ cd ..
 pip3 install cryptography
 wget https://raw.githubusercontent.com/sds2019/centos_script/master/simple_rmi_for_python
 cat simple_rmi_for_python  >> ~/.vimrc
-echo "===================================python 3 installed"
+echo "===================================python 3 installed\n\n\n"
 wget https://files.pythonhosted.org/packages/1d/64/a18a487b4391a05b9c7f938b94a16d80305bf0369c6b0b9509e86165e1d3/setuptools-41.0.1.zip
 unzip setuptools-41.0.1.zip 
 cd setuptools-41.0.1
 python3 setup.py  build
 python3 setup.py install
 cd ..
-echo "=======================================installed: setuptools for python"
+echo "=======================================installed: setuptools for python\n\n\n"
 yum install perl -y
 echo "=====================================perl  installed"
 wget http://mirrors.tuna.tsinghua.edu.cn/apache/maven/maven-3/3.6.1/binaries/apache-maven-3.6.1-bin.tar.gz
@@ -60,8 +60,8 @@ echo "#!/bin/bash\nexport M2_HOME=/usr/local/lib/apache-maven-3.6.1\nexport PATH
 chmod 0744 /etc/profile.d/maven.sh
 source /etc/profile.d/maven.sh
 mvn -v
-echo "=====================================mvn  installed"
+echo "=====================================mvn  installed\n\n\n"
 
 echo "su cozy source ~/boot.sh" >>/etc/rc.local
 echo "echo HELLO FROM boot.sh" >> /home/cozy/boot.sh
-echo "=====================================cozy initial boot script done."
+echo "=====================================cozy initial boot script done.\n\n\n"
